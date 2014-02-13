69d6c6f218cb0d9708d8dd8e2e16d8f571c8b899
yonghuang <h00138136@163.com>
2014-02-13 19:21:59 +0800

    update index page

--- app.js
+++ app.js
@@ -6,12 +6,21 @@
 var express = require('express');
 var routes = require('./routes');
 var http = require('http');
+var path = require('path');
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.use(express.logger('dev'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.multipart());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
@@ -20,6 +29,6 @@ if ('development' == app.get('env')) {
 
 routes(app);
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), '192.168.56.101', function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
