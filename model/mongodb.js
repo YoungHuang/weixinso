420cf66ac67afccb2761c245b49e713bdf15fdf9
yonghuang <h00138136@163.com>
2014-02-17 17:51:58 +0800

    add create.ejs view page

--- model/mongodb.js
+++ model/mongodb.js
@@ -2,5 +2,34 @@
  * Created by jiesun on 13-12-24.
  */
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://192.168.56.102/test');
+
+if(process.env.VCAP_SERVICES){
+	console.log(process.env.VCAP_SERVICES);
+    var env = JSON.parse(process.env.VCAP_SERVICES);
+    var mongo = env['mongodb2-2.4.8'][0]['credentials'];
+}
+else{
+    var mongo = {
+    "hostname":"localhost",
+    "port":27017,
+    "username":"",
+    "password":"",
+    "name":"",
+    "db":"db"
+    }
+}
+var generate_mongo_url = function(obj){
+    obj.hostname = (obj.hostname || 'localhost');
+    obj.port = (obj.port || 27017);
+    obj.db = (obj.db || 'test');
+    if(obj.username && obj.password){
+        return "mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db;
+    }
+    else{
+        return "mongodb://" + obj.hostname + ":" + obj.port + "/" + obj.db;
+    }
+}
+var mongourl = generate_mongo_url(mongo);
+mongoose.connect(mongourl);
+
 exports.mongoose = mongoose;
